c356ecf2c0556c843baf1776f9b838795707d693
Muhammad Zeeshan <muhammad.zeeshan@ldas-pcdev1.ligo.caltech.edu>
2023-05-07 07:37:56 -0700

    run.sh : Adding time command in bash script

---
+++ run-10-20-1000.sh
@@ -0,0 +1,1 @@
+time python new_powerlaw_mcmc.py events/GW150914_GWTC-1.dat events/GW151012_GWTC-1.dat events/GW151226_GWTC-1.dat events/GW170104_GWTC-1.dat events/GW170608_GWTC-1.dat events/GW170729_GWTC-1.dat events/GW170809_GWTC-1.dat events/GW170814_GWTC-1.dat events/GW170818_GWTC-1.dat events/GW170823_GWTC-1.dat vt_m1_m2.hdf5 myout_10_20_1000.hdf5 --n-walkers 20 --n-samples 1000
