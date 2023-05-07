3243853a649b5b699fe596ff6c57d256ce06c318
Muhammad Zeeshan <muhammad.zeeshan@ldas-pcdev1.ligo.caltech.edu>
2023-05-07 06:40:24 -0700

    Adding Bash file to run the code

---
+++ run-10-50-5000.sh
@@ -0,0 +1,1 @@
+python new_powerlaw_mcmc.py events/GW150914_GWTC-1.dat events/GW151012_GWTC-1.dat events/GW151226_GWTC-1.dat events/GW170104_GWTC-1.dat events/GW170608_GWTC-1.dat events/GW170729_GWTC-1.dat events/GW170809_GWTC-1.dat events/GW170814_GWTC-1.dat events/GW170818_GWTC-1.dat events/GW170823_GWTC-1.dat vt_m1_m2.hdf5 myout_10_50_5000.hdf5 --n-walkers 50 --n-samples 5000
