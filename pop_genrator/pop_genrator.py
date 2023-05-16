17948c73e1a775b4a8d6326d14a4b97c33c590a2
Muhammad Zeeshan <muhammad.zeeshan@ldas-pcdev2.ligo.caltech.edu>
2023-05-16 10:08:25 -0700

    Modifying the output in bash script and pop_genrator

--- pop_genrator/pop_genrator.py
+++ pop_genrator/pop_genrator.py
@@ -8,6 +8,17 @@ array = syn_pop_prob.joint_rvs(100,-2,5,50,200)
 
 col_names = ["m1_source", "m2_source", "ecc"]
 
+#saving array in a single file
+
+np.savetxt("combine_pop.txt", array, header="\t".join(col_names))
+
+# saving each row into a seprate file
+
+for i in range(array.shape[0]):
+    filename = f"row_{i+1}.txt"
+    row = array[i,:]
+    np.savetxt(filename, [row], delimiter="\t", fmt="%.3f",header="\t".join(col_names))
+
 # Saving each row of the array to a separate file with column names
 
 for i in range(array.shape[0]):
--- pop_genrator/run_genrator.sh
+++ pop_genrator/run_genrator.sh
@@ -1,1 +1,1 @@
-python pop_genrator.py ; rename txt dat *.txt ; mkdir dat_files ; mv *.dat ./dat_files
+python pop_genrator.py ; rename txt dat *.txt ; mkdir dat_events_files ;mkdir dat_row_files ;  mv eve* ./dat_events_files ; mv row* ./dat_row_files
