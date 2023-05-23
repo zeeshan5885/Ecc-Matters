5db006a9c304d58c86cf735063ba63563b0e8927
Muhammad Zeeshan <muhammad.zeeshan@ldas-grid.ligo.caltech.edu>
2023-05-23 13:12:56 -0700

    adding script file after correction

---
+++ run.sh
@@ -0,0 +1,1 @@
+time python new_powerlaw_mcmc.py events/GW150914_GWTC-1.dat events/GW151012_GWTC-1.dat events/GW151226_GWTC-1.dat events/GW170104_GWTC-1.dat events/GW170608_GWTC-1.dat events/GW170729_GWTC-1.dat events/GW170809_GWTC-1.dat events/GW170814_GWTC-1.dat events/GW170818_GWTC-1.dat events/GW170823_GWTC-1.dat vt_m1_m2.hdf5 myout_10_120_20000.hdf5 --n-walkers 120 --n-samples 20000
