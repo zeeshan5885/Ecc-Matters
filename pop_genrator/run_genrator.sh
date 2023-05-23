a6e37634e44b8f5dc3de6c6f842c6b0093b70098
Muhammad Zeeshan <muhammad.zeeshan@ldas-pcdev2.ligo.caltech.edu>
2023-05-23 09:23:58 -0700

    correcting the script for generalization

--- pop_genrator/run_genrator.sh
+++ pop_genrator/run_genrator.sh
@@ -1,2 +1,2 @@
 rm combine_pop.txt ; rm -r txt_events ; rm -r dat_events ; rm -r __pycache__ ;
-python population_gen.py ; mkdir txt_events ; mkdir dat_events; mv eve* ./txt_events ; cd txt_events ; cp eve* /home/muhammad.zeeshan/ms-work/ms-project/pop_genrator/dat_events ; cd .. ; cd dat_events;  rename txt dat *.txt
+python population_gen.py ; mkdir txt_events ; mkdir dat_events; mv eve* ./txt_events ; cd txt_events ; cp eve* ../dat_events ; cd .. ; cd dat_events;  rename txt dat *.txt
