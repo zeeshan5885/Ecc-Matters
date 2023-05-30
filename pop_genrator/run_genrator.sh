f3aa10ea6b60e6b5b26a88471c892f0684ee7859
Muhammad Zeeshan <muhammad.zeeshan@ldas-grid.ligo.caltech.edu>
2023-05-30 06:59:28 -0700

    Adding New population genration directory after correction, it also have the 8 poulations files which are going to run for our analysis

--- pop_genrator/run_genrator.sh
+++
@@ -1,2 +0,0 @@
-rm combine_pop.txt ; rm -r txt_events ; rm -r dat_events ; rm -r __pycache__ ;
-python population_gen.py ; mkdir txt_events ; mkdir dat_events; mv eve* ./txt_events ; cd txt_events ; cp eve* ../dat_events ; cd .. ; cd dat_events;  rename txt dat *.txt
