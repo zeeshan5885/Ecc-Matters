0f2e7bc4a2b879ce6d2eb90a5745fcd67b2e43a9
Muhammad Zeeshan <muhammad.zeeshan@ldas-pcdev12.ligo.caltech.edu>
2023-06-02 08:18:19 -0700

    Correcting the data shape for eccentricity

--- new_powerlaw_mcmc.py
+++ new_powerlaw_mcmc.py
@@ -71,8 +71,8 @@ def expval_mc(
 
         m1 = m1_m2[:,0]
         m2 = m1_m2[:,1]
-
-        return raw_interpolator(m1_m2[:,:1])
+        print(m1_m2.shape,m1_m2)
+        return raw_interpolator(m1_m2[:,:2])
 
     I, err_abs, err_rel = mc.integrate_adaptive(
         p, efficiency_fn,
