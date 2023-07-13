485774fe68ebc3466222e86d97b4f310d3c6187b
Muhammad Zeeshan <muhammad.zeeshan@ldas-pcdev12.ligo.caltech.edu>
2023-07-13 10:28:04 -0700

    Correcting run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-time python -mcProfile new_powerlaw_mcmc.py events/GW150914_GWTC-1.dat vt_m1_m2.hdf5 myout.hdf5 --n-walkers 20 --n-samples 30
+time python -mcProfile new_powerlaw_mcmc.py events/*.dat vt_m1_m2.hdf5 myout.hdf5 --n-walkers 20 --n-samples 30
