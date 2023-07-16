2fe929e16e8e1e923b89526e91a7e8f8788c9089
Muhammad Zeeshan <muhammad.zeeshan@ldas-pcdev2.ligo.caltech.edu>
2023-07-15 18:27:01 -0700

    moving populations plots from jupyter to python file

--- population-genrator/plotting.py
+++ population-genrator/plotting.py
@@ -8,7 +8,8 @@ Created on Thu Jun 15 21:33:41 2023
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-
+from mpl_toolkits.mplot3d import Axes3D
+import mplcursors
 # Get a list of all files matching the pattern
 file_list = glob.glob("./rPE_events/event_*.dat")
 
@@ -28,8 +29,8 @@ plt.title("Scatter Plot")
 plt.xlabel("$m_1 [M_\odot]$")
 plt.ylabel("$m_2 [M_\odot]$")
 #plt.legend()  # Display legend
-plt.show()
 plt.savefig("scatter_plot.png")
+plt.figure()
 
 
 # Plotting the whole population with median and std of each event
@@ -39,6 +40,51 @@ plt.rc('lines', **lines)
 plt.xlabel("$m_1 [M_\odot]$")
 plt.ylabel("$m_2 [M_\odot]$")
 plt.plot(combined_data[:,0], combined_data[:,1], 'ro', markersize=10)
-plt.show()
 plt.savefig("mean_masses.png")
+plt.figure()
 
+# making population plots
+# Read the data from the text file
+data = np.loadtxt("weighted_population.dat")
+# Extract the columns
+m1 = data[:, 0]
+m2 = data[:, 1]
+ecc = data[:, 2]
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the data points as dots with colors
+sc = ax.scatter(m1, m2, ecc, c=ecc, cmap='plasma', marker='o')
+
+# Set labels for the axes
+ax.set_xlabel('$m_1[M_\odot]$')
+ax.set_ylabel('$m_2[M_\odot]$')
+ax.set_zlabel('$\epsilon$')
+
+# Set a title for the plot
+#ax.set_title('Eccentric Synthetic Population')
+
+# Add interactivity with mplcursors
+cursors = mplcursors.cursor(sc, hover=True)
+cursors.connect("add", lambda sel: sel.annotation.set_text(f"({sel.target[0]:.2f}, {sel.target[1]:.2f}, {sel.target[2]:.2f})"))
+
+# Add a colorbar
+cbar = fig.colorbar(sc)
+cbar.set_label('$\epsilon$')
+plt.savefig('pop3d_0.05.png', bbox_inches='tight')
+# Display the plot
+plt.figure()
+
+scaled_data = np.loadtxt("scaled_population.dat")
+scaled_m1 = scaled_data[:,0]
+scaled_m2 = scaled_data[:,1]
+plt.plot(m1,m2,'o',label = 'EBBHs')
+plt.plot(scaled_m1,scaled_m2,'.',label = 'NEBBHs')
+plt.legend()
+plt.xlabel('$m_1 [M_\odot]$')
+plt.ylabel('$m_2[M_\odot]$')
+#plt.title('scaled m1 vs scaled m2')
+plt.savefig('pop2d_0.05.png', bbox_inches='tight')
+plt.figure()
