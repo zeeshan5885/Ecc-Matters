3ceaae67f052b948682543f1d1a0b63b2e2e68dc
Muhammad Zeeshan <58973922+zeeshan5885@users.noreply.github.com>
2023-08-15 16:20:53 -0400

    formating

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-time python3 -mcProfile new_powerlaw_mcmc.py events/*.dat vt_m1_m2.hdf5 myout.hdf5 --n-walkers 10 --n-samples 400;
+time python3 -mcProfile new_powerlaw_mcmc.py weighted_events/*.dat vt_m1_m2.hdf5 myout.hdf5 --n-walkers 10 --n-samples 400;
 mv myout.hdf5 ./plotting/
