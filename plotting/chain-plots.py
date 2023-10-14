69c4dbe3e86f4cd0e555aa85ed73c3b0e75e209e
M. Abbas Haider <abbastaqvi81@gmail.com>
2023-10-14 20:45:04 +0500

    fix run.sh and add plots.png in plotting (blank tho)

--- plotting/chain-plots.py
+++ plotting/chain-plots.py
@@ -2,54 +2,54 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-data1 = h5py.File("myout.hdf5", "r")
+# data1 = h5py.File("myout.hdf5", "r")
 data2 = h5py.File("myout_org.hdf5", "r")
-data_3D1 = np.array(data1["pos"])
+# data_3D1 = np.array(data1["pos"])
 data_3D2 = np.array(data2["pos"])
 fig, axes = plt.subplots(5, figsize=(12, 12), sharex=True)
-samples = data_3D1
-labels = [
-    "$log_{10}(\mathcal{R})$",
-    r"$\alpha$",
-    "$m_{min}$",
-    "$m_{max}$",
-    "$\sigma_\epsilon$",
-]
-
-for i in range(5):
-    ax = axes[i]
-    ax.plot(samples[:, :, i], "-", alpha=0.3)
-    ax.set_xlim(0, len(samples))
-    ax.set_ylabel(labels[i])
-    plt.savefig("chain.png")
-
-axes[-1].set_xlabel("step number")
-
-plt.figure()
-plt.plot(samples[:, :, 0], "-", color="k")
-plt.xlabel("step number")
-plt.ylabel("$log_{10}(\mathcal{R})$")
-plt.savefig("rate.png")
-plt.figure()
-plt.plot(samples[:, :, 1], "-", color="k")
-plt.xlabel("step number")
-plt.ylabel(r"$\alpha$")
-plt.savefig("alpha.png")
-plt.figure()
-plt.plot(samples[:, :, 2], "-", color="k")
-plt.xlabel("step number")
-plt.ylabel(r"$m_{min}$")
-plt.savefig("min.png")
-plt.figure()
-plt.plot(samples[:, :, 3], "-", color="k")
-plt.xlabel("step number")
-plt.ylabel(r"$m_{max}$")
-plt.savefig("max.png")
-plt.figure()
-plt.plot(samples[:, :, 4], "-", color="k")
-plt.xlabel("step number")
-plt.ylabel("$\sigma_\epsilon$")
-plt.savefig("sigecc.png")
+# samples = data_3D1
+# pabels = [
+#     "$log_{10}(\mathcal{R})$",
+#     r"$\alpha$",
+#     "$m_{min}$",
+#     "$m_{max}$",
+#     "$\sigma_\epsilon$",
+# ]
+#
+# for i in range(5):
+#     ax = axes[i]
+#     ax.plot(samples[:, :, i], "-", alpha=0.3)
+#     ax.set_xlim(0, len(samples))
+#     ax.set_ylabel(labels[i])
+#     plt.savefig("chain.png")
+#
+# axes[-1].set_xlabel("step number")
+#
+# plt.figure()
+# plt.plot(samples[:, :, 0], "-", color="k")
+# plt.xlabel("step number")
+# plt.ylabel("$log_{10}(\mathcal{R})$")
+# plt.savefig("rate.png")
+# plt.figure()
+# plt.plot(samples[:, :, 1], "-", color="k")
+# plt.xlabel("step number")
+# plt.ylabel(r"$\alpha$")
+# plt.savefig("alpha.png")
+# plt.figure()
+# plt.plot(samples[:, :, 2], "-", color="k")
+# plt.xlabel("step number")
+# plt.ylabel(r"$m_{min}$")
+# plt.savefig("min.png")
+# plt.figure()
+# plt.plot(samples[:, :, 3], "-", color="k")
+# plt.xlabel("step number")
+# plt.ylabel(r"$m_{max}$")
+# plt.savefig("max.png")
+# plt.figure()
+# plt.plot(samples[:, :, 4], "-", color="k")
+# plt.xlabel("step number")
+# plt.ylabel("$\sigma_\epsilon$")
+# plt.savefig("sigecc.png")
 
 # The following chain plots are for the NEBBHs
 samples1 = data_3D2
