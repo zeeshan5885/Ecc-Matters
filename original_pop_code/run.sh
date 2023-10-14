7ec24cfd8dafdf716f0e3ec113e379784628bf16
M. Abbas Haider <abbastaqvi81@gmail.com>
2023-10-14 21:06:36 +0500

    set samples to 100 with 20 walkers and update plots

--- original_pop_code/run.sh
+++ original_pop_code/run.sh
@@ -1,2 +1,2 @@
-time python3 -mcProfile original_powerlaw_mcmc.py scaled_events/*.dat vt_m1_m2.hdf5 myout_org.hdf5 --n-walkers 10 --n-samples 1
+time python3 -mcProfile original_powerlaw_mcmc.py scaled_events/*.dat vt_m1_m2.hdf5 myout_org.hdf5 --n-walkers 20 --n-samples 100
 mv myout_org.hdf5 ../plotting/
